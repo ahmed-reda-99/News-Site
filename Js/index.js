c96108e96eaa9433d6b948c2f6f86dfa9130a1bb
ahmed-reda-99 <ahmed.avy@gmail.com>
2019-09-29 14:35:05 +0200

    add coments in code

--- Js/index.js
+++ Js/index.js
@@ -1,80 +1,77 @@
-var links = document.getElementsByClassName("nav-link");
-var searchInp = document.getElementById("searchInp");
-var news ;
-var category = 'general';
-var country = 'eg';
+          /* declarition Variables*/
 
-getNews();
-
-
-
-for(var i=0 ; i <links.length ; i++)
-    {
-        links[i].addEventListener("click" , function(e){
-            category = e.target.innerHTML;
-            getNews();
-        })
-    }
+          var links = document.getElementsByClassName("nav-link");
+          var searchInp = document.getElementById("searchInp");
+          var news;
+          var category = 'general';
+          var country = 'eg';
 
 
 
+          getNews();
+
+          for (var i = 0; i < links.length; i++) {
+              links[i].addEventListener("click", function (e) {
+                  category = e.target.innerHTML;
+                  getNews();
+              })
+          }
 
 
 
-function getNews()
-{
 
-    var req ; // IE5 , IE6 
 
-    if(window.XMLHttpRequest) // modern browsers 
-        {
-                req = new XMLHttpRequest();
-        }
-    else // IE5 ,IE6
-        {
-               req = new ActiveXObject("Microsoft.XMLHTTP") 
-        }    
+          /*getNews*/
 
-    var url =`https://newsapi.org/v2/top-headlines?country=`+country+`&category=`+category+`&apiKey=d34d49ce3a794aca80d1ae821239b0eb`
-    req.open("GET",url )
 
-    req.onreadystatechange =function()
-    {
-        if(req.status == 200 &&  req.readyState == 4)
-            {
+          function getNews() {
 
-            news =  JSON.parse (req.response );
-            news = news.articles;
-            displayNews();
-            }
-    }
+              var req; // IE5 , IE6 
 
-    req.send();
+              if (window.XMLHttpRequest) // modern browsers 
+              {
+                  req = new XMLHttpRequest();
+              } else // IE5 ,IE6
+              {
+                  req = new ActiveXObject("Microsoft.XMLHTTP")
+              }
 
-}
-function displayNews()
-{
-    var temp = "";
-    for(var i=0 ; i < news.length ; i++)
-    {
-        var title =news[i].title
-        var titleSub = title.substring(0,40)
-        var desc = news[i].description
-        temp +=` <div class="col-lg-3 md-6 sm-12">
+              var url = `https://newsapi.org/v2/top-headlines?country=` + country + `&category=` + category + `&apiKey=d34d49ce3a794aca80d1ae821239b0eb`
+              req.open("GET", url)
+
+              req.onreadystatechange = function () {
+                  if (req.status == 200 && req.readyState == 4) {
+
+                      news = JSON.parse(req.response);
+                      news = news.articles;
+                      displayNews();
+                  }
+              }
+
+              req.send();
+
+          }
+
+
+          /*displayNews*/
+
+
+          function displayNews() {
+              var temp = "";
+              for (var i = 0; i < news.length; i++) {
+                  var title = news[i].title
+                  var titleSub = title.substring(0, 40)
+                  var desc = news[i].description
+                  temp += ` <div class="col-lg-3 md-6 sm-12">
         <div class="new bg-warning text-center  justify-content-center">
-        <a href=`+news[i].url+`>
+        <a href=` + news[i].url + `>
         <div class="imgNews">
-        <img src="`+news[i].urlToImage+`" alt="NewsImg" class="img-fluid"/></div>
-                <h5 class="">`+titleSub+`...</h5></a>
-                <p class="text-muted">`+desc+`...</p>
+        <img src="` + news[i].urlToImage + `" alt="NewsImg" class="img-fluid"/></div>
+                <h5 class="">` + titleSub + `...</h5></a>
+                <p class="text-muted">` + desc + `...</p>
         </div>
         <br/>
     </div>`
-    }
-    document.getElementById("newsRow").innerHTML = temp;   
-}
-
-
-
-
-
+              }
+              document.getElementById("newsRow").innerHTML = temp;
+          }
